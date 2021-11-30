8ec697d79227ae86989a1189f992c4ea6715d21d
Raúl Gómez <rgomez@voxelgroup.net>
2021-11-30 09:44:25 +0100

    chore: Create Projects

---
+++ PasswordValidationSR.Tests/PasswordValidatorShould.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace PasswordValidationSR.Tests
+{
+    public class PasswordValidatorShould
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
